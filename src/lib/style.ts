49dda3dfac061b92c66914ec7b3753945a6c7eef
cyanChill <83375816+cyanChill@users.noreply.github.com>
2025-11-06 21:55:14 -0500

    chore: Add expanded neutral & red color offering
    - Colors obtained from https://www.youtube.com/watch?v=XpSw0s3Kr3A.

--- src/lib/style.ts
+++ src/lib/style.ts
@@ -2,10 +2,24 @@ import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { extendTailwindMerge } from "tailwind-merge";
 
+const sharedColorPalette = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+const nColorPalette = [...sharedColorPalette, 0, 15, 95, 100];
+const rColorPalette = [...sharedColorPalette, 55];
+
 const customTwMerge = extendTailwindMerge({
   extend: {
     theme: {
-      color: ["canvas", "surface", "content", "accent"],
+      color: [
+        "canvas",
+        "surface",
+        "content",
+        "accent",
+        nColorPalette.map((light) => `n-${light}`),
+        rColorPalette.map((light) => `r-${light}`),
+        "y",
+        "b",
+        "g",
+      ].flat(),
     },
   },
 });
